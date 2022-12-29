76119d522801f16c9917c3ad3043a54dc6b64d24
jean antony julio miranda cruz <jhean1695@gmail.com>
2022-12-28 20:23:52 -0500

    CAMBIOS
    :D

--- quiz.js
+++ quiz.js
@@ -1,122 +1,117 @@
-const data= [
+const data = [
     {
-        id:1,
-        question:"cual es una pez espada en ingles?",
-        answers:[
-            {answers : "jellyfish",isCorrect:false},
-            {answers : "swordfish",isCorrect:true},
-            {answers : "starfish",isCorrect:false},
-            {answers : "crayfish",isCorrect:false},
-        ],
+      id: 1,
+      question: "Which of these fish is actually a fish?",
+      answers: [
+        { answer: "swordfish", isCorrect: true },
+        { answer: "jellyfish", isCorrect: false },
+        { answer: "starfish", isCorrect: false },
+        { answer: "crayfish", isCorrect: false },
+      ],
     },
     {
-        id:2,
-        question:"cual es una pez espada en ingles?",
-        answers:[
-            {answers : "jelly fish",isCorrect:false},
-            {answers : "jelly fish",isCorrect:false},
-            {answers : "jelly fish",isCorrect:false},
-            {answers : "jelly fish",isCorrect:false},
-        ],
+      id: 2,
+      question: "A flutter is a group of:",
+      answers: [
+        { answer: "bees", isCorrect: false },
+        { answer: "penguins", isCorrect: false },
+        { answer: "butterflies", isCorrect: true },
+        { answer: "camels", isCorrect: false },
+      ],
     },
     {
-        id:3,
-        question:"cual es una pez espada en ingles?",
-        answers:[
-            {answers : "jelly fish",isCorrect:false},
-            {answers : "jelly fish",isCorrect:false},
-            {answers : "jelly fish",isCorrect:false},
-            {answers : "jelly fish",isCorrect:false},
-        ],
+      id: 1,
+      question: "A group of which animals is referred to as a wake?",
+      answers: [
+        { answer: "bats", isCorrect: false },
+        { answer: "vultures", isCorrect: true },
+        { answer: "ants", isCorrect: false },
+      ],
     },
-
-];
-
-const gameScreen = document.querySelector(".game");
-const resultScreen =document.querySelector(".result");
-const question =document.querySelector(".question");
-const answersContainer =document.querySelector(".answer");
-const submit =document.querySelector(".submit");
-const play =document.querySelector(".play");
-
-
-let qIndex = 0;
-let correctCount = 0;
-let wrongCount =0;
-let total = 0;
-let selectedAnswer;
-
-const playAgain = () =>{
+  ];
+  
+  const gameScreen = document.querySelector(".game");
+  const resultScreen = document.querySelector(".result");
+  const question = document.querySelector(".question");
+  const answersContainer = document.querySelector(".answers");
+  const submit = document.querySelector(".submit");
+  const play = document.querySelector(".play");
+  
+  let qIndex = 0;
+  let correctCount = 0;
+  let wrongCount = 0;
+  let total = 0;
+  let selectedAnswer;
+  
+  const playAgain = () => {
     qIndex = 0;
-    correctCount =0;
-    wrongCount =0;
-    total =0;
+    correctCount = 0;
+    wrongCount = 0;
+    total = 0;
     showQuestion(qIndex);
-};
-
-play.addEventListener("click",()=>{
+  };
+  
+  play.addEventListener("click",()=>{
     resultScreen.style.display = "none";
     gameScreen.style.display = "block";
     playAgain()
-})
-
-const showResult = () => {
+  })
+  
+  const showResult = () => {
     resultScreen.style.display = "block";
     gameScreen.style.display = "none";
-
-
+  
     resultScreen.querySelector(
-        ".correct"
-        ).textContent = `Correct answers: ${correctCount}`;
-
+      ".correct"
+    ).textContent = `Correct Answers: ${correctCount}`;
+  
     resultScreen.querySelector(
-            ".wrong"
-            ).textContent = `Correct answers: ${wrongCount}`;
-
+      ".wrong"
+    ).textContent = `Wrong Answers: ${wrongCount}`;
+  
     resultScreen.querySelector(".score").textContent = `Score: ${
-        (correctCount - wrongCount) * 10
+      (correctCount - wrongCount) * 10
     }`;
-};
-
-const showQuestion = (qNumber) =>{
+  };
+  
+  const showQuestion = (qNumber) => {
     if (qIndex === data.length) return showResult();
     selectedAnswer = null;
     question.textContent = data[qNumber].question;
     answersContainer.innerHTML = data[qNumber].answers
-    .map(
-        (item,index) =>
+      .map(
+        (item, index) =>
+          `
+    <div class="answer">
+        <input type="radio" id=${index} name="answer" value=${item.isCorrect} />
+        <label for="1">${item.answer}</label>
+    </div>
+    `
+      )
+      .join("");
+  
+    selectAnswer();
+  };
+  
+  const selectAnswer = () => {
+    answersContainer.querySelectorAll("input").forEach((el) => {
+      el.addEventListener("click", (e) => {
+        selectedAnswer = e.target.value;
+      });
+    });
+  };  
+ 
 
-        `
-        <div class="answer">
-         <input type="radio" id=${index} name="answer" value=${item.isCorrect} />
-         <label for='1'>${item.answers}</label>
-         <div>
-        ` )
-        .join("");
-
-        selectAnswer();
-    };
-
-    const selectAnswer = () => {
-        answersContainer.querySelectorAll("input").forEach((el) =>{
-            el.addEventListener("click",(e) =>{
-                selectedAnswer = e.target.value;
-            });
-        });
-    };    
-
-    const submitAnswer = () => {
-        submit.querySelectorAll("click", (el) =>{
-            if (selectedAnswer !== null) {
-                selectedAnswer === "true" ? correctCount++ : wrongCount++;
-                qIndex++;
-
-                showQuestion(qIndex);
-            } else alert("select an nswer!");
-        });
-    };
-
-    showQuestion(qIndex);
-    submitAnswer();
-
-
+  
+  const submitAnswer = () => {
+    submit.addEventListener("click", () => {
+      if (selectedAnswer !== null) {
+        selectedAnswer === "true" ? correctCount++ : wrongCount++;
+        qIndex++;
+        showQuestion(qIndex);
+      } else alert("Select an answer!");
+    });
+  };
+  
+  showQuestion(qIndex);
+  submitAnswer();
